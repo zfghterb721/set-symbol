16296a19ce77f87fc1d9bde4b2daaf23d2e427b6
Barrett Routon <barrett@binderpos.com>
2024-05-02 22:33:46 -0500

    Fixed Conflux and order of operations in set lookup

--- src/setNameDictionary.ts
+++ src/setNameDictionary.ts
@@ -9,13 +9,20 @@ type SetNameDictionary = {
 };
 
 export const getSetCode = (setName: string): setKeys | undefined => {
-    if(Object.keys(svg_data_json).includes(setName)){
-        return setName as setKeys;
+    //These are thre overrides for sets that have different names in the svg_data.json file
+    if(Object.keys(setNameDictionary).includes(setName)){
+        return setNameDictionary[setName];
     }
+    //this checks mtg json set names and grabs the keyrune code
     if(Object.keys(mtg_json).includes(setName)){
         return mtg_json[setName];
     }
-    return setNameDictionary[setName];
+    //this checks if the passed param is just the keyrune code and returns it
+    if(Object.keys(svg_data_json).includes(setName)){
+        return setName as setKeys;
+    }
+
+    return undefined;
 }
 
 
@@ -24,4 +31,6 @@ const setNameDictionary: SetNameDictionary = {
     "Wilds of Eldraine": "woe",
     "Strixhaven: School of Mages": "stx",
     "Strixhaven: School of Mages Mystical Archive": "sta",
+    "Conflux":"con_",
+    "con":"con_",
 };
