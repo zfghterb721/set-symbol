e1c4faf8c0ff64133ddb27e9c7812e10521e86d7
SoupeWonton <112906940+SoupeWonton@users.noreply.github.com>
2025-06-03 15:10:25 -0400

    Update setNameDictionary.ts
    adding TDM to dictionary

--- src/setNameDictionary.ts
+++ src/setNameDictionary.ts
@@ -83,6 +83,7 @@ const setNameDictionary: SetNameDictionary = {
     'Modern Horizons 3 Commander': 'm3c',
     'Outlaws of Thunder Junction: The Big Score': 'big',
     'Outlaws of Thunder Junction: Breaking News': 'otp',
+    'Tarkir: Dragonstorm': 'tdm',
     'The Lord of the Rings: Tales of Middle-Earth': 'ltr',
     'The Lord of the Rings: Tales of Middle-Earth Showcase Scrolls': 'ltr',
     'The Lord of the Rings: Tales of Middle-Earth Commander': 'ltc',
